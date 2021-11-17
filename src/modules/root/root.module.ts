89fd933d082839f83288a096e2148f5a1ce4a748
Shivansh Kuchchal <skuchcha@redhat.com>
2021-11-17 18:49:20 +0530

    Added service workers.

--- src/modules/root/root.module.ts
+++ src/modules/root/root.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from '../../environments/environment';
 import { SharedModule } from '../shared/shared.module';
 import { RootComponent } from './root.component';
 import { routes } from './root.router';
 
 @NgModule({
   declarations: [RootComponent],
-  imports: [BrowserAnimationsModule, BrowserModule, RouterModule.forRoot(routes), SharedModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [RootComponent],
 })
