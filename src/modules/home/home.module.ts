7b11cac5c72c04db0f5b2e062cf5af595fdb2a13
Shivansh Kuchchal <skuchcha@redhat.com>
2021-11-18 17:55:43 +0530

    Input UI layer done.

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -7,11 +7,14 @@ import { ChatMetaStoreService } from '../../services/chat-meta-store/chat-meta-s
 import { AbstractProfileStoreService } from '../../services/profile-store/profile-store.abstract';
 import { ProfileStoreService } from '../../services/profile-store/profile-store.service';
 import { MaterialModule } from '../material/material.module';
+import { ChatBoxComponent } from './components/chat-box/chat-box.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { InputComponent } from './components/input/input.component';
 import { HomeComponent } from './home.component';
 import { routes } from './home.router';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ChatListComponent, ChatBoxComponent, InputComponent],
   imports: [CommonModule, MaterialModule, RouterModule.forChild(routes)],
   providers: [
     { provide: AbstractChatMetaStoreService, useClass: ChatMetaStoreService },
