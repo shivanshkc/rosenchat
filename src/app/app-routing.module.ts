7219ea03cad87761fa6de4aed10e7d2052dbfcee
Shivansh Kuchchal <shivanshbox@gmail.com>
2022-12-31 16:51:32 +0530

    feat(core): add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'landing',
+    // eslint-disable-next-line @typescript-eslint/promise-function-async
+    loadChildren: () => import('src/app/modules/landing/landing.module').then((m) => m.LandingModule)
+    // canActivate: [MustBeLoggedOutService],
+  },
+  {
+    path: 'auth/callback',
+    // eslint-disable-next-line @typescript-eslint/promise-function-async
+    loadChildren: () => import('src/app/modules/auth-callback/auth-callback.module').then((m) => m.AuthCallbackModule)
+    // canActivate: [MustBeLoggedOutService],
+  },
+  {
+    path: 'home',
+    // eslint-disable-next-line @typescript-eslint/promise-function-async
+    loadChildren: () => import('src/app/modules/home/home.module').then((m) => m.HomeModule)
+    // canActivate: [MustBeLoggedInService],
+  },
+  { path: '**', redirectTo: 'home' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/landing/landing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { LandingComponent } from './landing.component'
+import { RouterModule, Routes } from '@angular/router'
+
+export const routes: Routes = [{ path: '**', component: LandingComponent }]
+
+@NgModule({
+  declarations: [
+    LandingComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class LandingModule { }
